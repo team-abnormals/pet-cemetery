e643d0f5d08b0088a7d4e6f9769d671aa3d723d5
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-01-29 15:36:47 -0600

    Slight clean

--- src/main/java/com/teamabnormals/pet_cemetery/common/advancement/RespawnAnimalTrigger.java
+++ src/main/java/com/teamabnormals/pet_cemetery/common/advancement/RespawnAnimalTrigger.java
@@ -3,13 +3,11 @@ package com.teamabnormals.pet_cemetery.common.advancement;
 import com.google.gson.JsonObject;
 import com.teamabnormals.pet_cemetery.common.advancement.RespawnAnimalTrigger.TriggerInstance;
 import com.teamabnormals.pet_cemetery.core.PetCemetery;
-import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.advancements.critereon.*;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.level.storage.loot.LootContext;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
 public class RespawnAnimalTrigger extends SimpleCriterionTrigger<TriggerInstance> {
 	private static final ResourceLocation ID = new ResourceLocation(PetCemetery.MOD_ID, "respawn_animal");
@@ -19,8 +17,8 @@ public class RespawnAnimalTrigger extends SimpleCriterionTrigger<TriggerInstance
 	}
 
 	public RespawnAnimalTrigger.TriggerInstance createInstance(JsonObject jsonObject, EntityPredicate.Composite entity, DeserializationContext context) {
-		EntityPredicate.Composite entitypredicate$composite = EntityPredicate.Composite.fromJson(jsonObject, "entity", context);
-		return new RespawnAnimalTrigger.TriggerInstance(entity, entitypredicate$composite);
+		EntityPredicate.Composite animal = EntityPredicate.Composite.fromJson(jsonObject, "entity", context);
+		return new RespawnAnimalTrigger.TriggerInstance(entity, animal);
 	}
 
 	public void trigger(ServerPlayer player, Animal animal) {
