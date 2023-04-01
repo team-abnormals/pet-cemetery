a838ecb8f13595618694658f443e29ece77fac47
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-04-01 15:53:12 -0500

    Fix Phantoms not attacking

--- src/main/java/com/teamabnormals/pet_cemetery/core/mixin/PhantomSweepAttackGoalMixin.java
+++ src/main/java/com/teamabnormals/pet_cemetery/core/mixin/PhantomSweepAttackGoalMixin.java
@@ -1,33 +1,23 @@
 package com.teamabnormals.pet_cemetery.core.mixin;
 
-import com.google.common.collect.Lists;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.MobType;
 import net.minecraft.world.entity.animal.Cat;
 import net.minecraft.world.entity.monster.Phantom.PhantomSweepAttackGoal;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 @Mixin(PhantomSweepAttackGoal.class)
 public class PhantomSweepAttackGoalMixin {
 
-	@Redirect(method = "canContinueToUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/animal/Cat;hiss()V"))
-	private void hiss(Cat cat) {
-		if (cat.getMobType() != MobType.UNDEAD)
-			cat.hiss();
-	}
-
-	@Redirect(method = "canContinueToUse", at = @At(value = "INVOKE", target = "Ljava/util/List;isEmpty()Z"))
-	private boolean isEmpty(List<Cat> cats) {
-		List<Cat> toRemove = Lists.newArrayList();
-		for (Cat cat : cats) {
-			if (cat.getMobType() == MobType.UNDEAD) {
-				toRemove.add(cat);
-			}
-		}
-		cats.removeAll(toRemove);
-		return !cats.isEmpty();
+	@Redirect(method = "canContinueToUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;getEntitiesOfClass(Ljava/lang/Class;Lnet/minecraft/world/phys/AABB;Ljava/util/function/Predicate;)Ljava/util/List;"))
+	private <T extends Entity> List<T> getEntitiesOfClass(Level level, Class<T> aClass, AABB aabb, Predicate<? super T> predicate) {
+		return level.getEntitiesOfClass(aClass, aabb, entity -> entity.isAlive() && entity instanceof Cat cat && cat.getMobType() != MobType.UNDEAD);
 	}
 }
