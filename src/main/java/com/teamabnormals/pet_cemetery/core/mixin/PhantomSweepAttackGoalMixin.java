def67ab146a25f78997a47566fa97bfb1bc08012
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2025-09-29 09:05:05 -0500

    Change mixins to use WrapOperation

--- src/main/java/com/teamabnormals/pet_cemetery/core/mixin/PhantomSweepAttackGoalMixin.java
+++ src/main/java/com/teamabnormals/pet_cemetery/core/mixin/PhantomSweepAttackGoalMixin.java
@@ -1,5 +1,7 @@
 package com.teamabnormals.pet_cemetery.core.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.animal.Cat;
 import net.minecraft.world.entity.monster.Phantom;
@@ -7,7 +9,6 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.List;
 import java.util.function.Predicate;
@@ -15,8 +16,8 @@ import java.util.function.Predicate;
 @Mixin(Phantom.PhantomSweepAttackGoal.class)
 public class PhantomSweepAttackGoalMixin {
 
-	@Redirect(method = "canContinueToUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;getEntitiesOfClass(Ljava/lang/Class;Lnet/minecraft/world/phys/AABB;Ljava/util/function/Predicate;)Ljava/util/List;"))
-	private <T extends Entity> List<T> getEntitiesOfClass(Level level, Class<T> aClass, AABB aabb, Predicate<? super T> predicate) {
-		return level.getEntitiesOfClass(aClass, aabb, entity -> predicate.test(entity) && entity instanceof Cat cat);
+	@WrapOperation(method = "canContinueToUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;getEntitiesOfClass(Ljava/lang/Class;Lnet/minecraft/world/phys/AABB;Ljava/util/function/Predicate;)Ljava/util/List;"))
+	private <T extends Entity> List<T> getEntitiesOfClass(Level level, Class<T> aClass, AABB aabb, Predicate<? super T> predicate, Operation<List<T>> original) {
+		return level.getEntitiesOfClass(aClass, aabb, entity -> predicate.test(entity) && entity instanceof Cat);
 	}
 }
--- src/main/java/com/teamabnormals/pet_cemetery/core/mixin/SitWhenOrderedToGoalMixin.java
+++ src/main/java/com/teamabnormals/pet_cemetery/core/mixin/SitWhenOrderedToGoalMixin.java
@@ -1,18 +1,22 @@
 package com.teamabnormals.pet_cemetery.core.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.teamabnormals.pet_cemetery.core.other.tags.PCEntityTypeTags;
 import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.ai.goal.SitWhenOrderedToGoal;
 import net.minecraft.world.level.block.Blocks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(SitWhenOrderedToGoal.class)
 public class SitWhenOrderedToGoalMixin {
 
-	@Redirect(method = "canUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/TamableAnimal;isInWaterOrBubble()Z"))
-	private boolean canUse(TamableAnimal animal) {
-		return (animal.getType().is(PCEntityTypeTags.ZOMBIE_PETS) || animal.getType().is(PCEntityTypeTags.SKELETON_PETS)) ? animal.level().getBlockState(animal.blockPosition()).is(Blocks.BUBBLE_COLUMN) : animal.isInWaterOrBubble();
+	@WrapOperation(method = "canUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/TamableAnimal;isInWaterOrBubble()Z"))
+	private boolean canUse(TamableAnimal animal, Operation<Boolean> original) {
+		if (animal.getType().is(PCEntityTypeTags.ZOMBIE_PETS) || animal.getType().is(PCEntityTypeTags.SKELETON_PETS)) {
+			return animal.level().getBlockState(animal.blockPosition()).is(Blocks.BUBBLE_COLUMN);
+		}
+		return original.call(animal);
 	}
 }
